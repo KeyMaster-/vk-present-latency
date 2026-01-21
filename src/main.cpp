4ba78d890a3fa18e4820c2092809e601bf2b71f9
Tilman Schmidt <AccessKeymaster@gmx.net>
2026-01-21 15:05:42 +0000

    Remove some wgl defines

--- src/main.cpp
+++ src/main.cpp
@@ -24,17 +24,6 @@
 #include "slang/slang.h"
 #include "slang/slang-com-ptr.h"
 
-// Missing defines from glad's wgl.h.
-#define WGL_CONTEXT_MAJOR_VERSION_ARB             0x2091
-#define WGL_CONTEXT_MINOR_VERSION_ARB             0x2092
-#define WGL_CONTEXT_LAYER_PLANE_ARB               0x2093
-#define WGL_CONTEXT_FLAGS_ARB                     0x2094
-#define WGL_CONTEXT_PROFILE_MASK_ARB              0x9126
-#define WGL_CONTEXT_DEBUG_BIT_ARB                 0x0001
-#define WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB    0x0002
-#define WGL_CONTEXT_CORE_PROFILE_BIT_ARB          0x00000001
-#define WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB 0x00000002
-
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 
